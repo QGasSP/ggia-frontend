50a565d729afb893a8fa4c05fed8796ce006c761
Onunda <vonunda@gmail.com>
2022-01-26 22:01:23 +0200

    [Legend: update names]

--- frontend_qgassp/src/components/Legend.jsx
+++ frontend_qgassp/src/components/Legend.jsx
@@ -8,14 +8,14 @@ import "../css/legend.css";
  */
 
 const itemLabels = [
-  { title: "Buses", color: "#8C0303" },
+  { title: "Bus", color: "#8C0303" },
   { title: "Metro", color: "#400D01" },
-  { title: "Passenger trains", color: "#D90404" },
-  { title: "Road freight", color: "#595959" },
-  { title: "Passenger cars", color: "#A6036D" },
-  { title: "Tram light train", color: " #C4D4F2" },
-  { title: "Rail (freight)", color: "#80D941" },
-  { title: "Inland waterways freight", color: "#F2CE1B" },
+  { title: "Train", color: "#D90404" },
+  { title: "Road transport", color: "#595959" },
+  { title: "Car", color: "#A6036D" },
+  { title: "Tram", color: " #C4D4F2" },
+  { title: "Rail transport", color: "#80D941" },
+  { title: "Water transport", color: "#F2CE1B" },
 ];
 
 export const Legend = () => {
