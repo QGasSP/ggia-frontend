400baf2a6889a81637ae5b7925a78c11fce04504
Armin Remenyi <remenyiarminlaszlo@gmail.com>
2022-03-04 18:04:50 +0200

    improvements on BE connection for U3 pages

--- frontend_qgassp/src/components/U3policies.jsx
+++ frontend_qgassp/src/components/U3policies.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { Button } from "./Button";
 import { useNavigate } from "react-router-dom";
 import "../css/u3.css";
-import axios from "axios";
 import {
   XYPlot,
   XAxis,
