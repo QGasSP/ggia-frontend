9e90cf183216843fbcb03380782c64a9a4bcc04b
Armin Remenyi <remenyiarminlaszlo@gmail.com>
2022-03-04 15:49:05 +0200

    resolving merge conflicts

---
+++ frontend_qgassp/src/AppRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import { NewResidents } from "./components/NewResidents";
+import { Settlement } from "./components/Settlement";
+import { StartPage } from "./components/StartPage";
+import { U1planner } from "./components/U1planner";
+import { U2planner } from "./components/U2planner";
+import { U3planner } from "./components/U3planner";
+import { U3policies } from "./components/U3policies";
+import { U1plannerDefault } from "./components/U1plannerDefault";
+import App from "./App";
+import { StackedBarchart } from "./components/StackedBarchart";
+
+export const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <App /> },
+    { path: "startPage", element: <StartPage /> },
+    { path: "settlement", element: <Settlement /> },
+    { path: "u1planner", element: <U1planner /> },
+    { path: "u1plannerDefault", element: <U1plannerDefault /> },
+    { path: "stackedBarChart", element: <StackedBarchart /> },
+    { path: "newResidents", element: <NewResidents /> },
+    { path: "u2planner", element: <U2planner /> },
+    { path: "u3planner", element: <U3planner /> },
+    { path: "u3policies", element: <U3policies /> },
+  ]);
+  return routes;
+};
