362baa50b43f8da3c1ddb08050fbe6fc8636f075
Onunda <vonunda@gmail.com>
2022-03-09 13:49:43 +0200

    add transport baseline

---
+++ frontend_qgassp/src/stories/TransportBaseline.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {TransportBaseline} from "../components/TransportBaseline";
+
+export default {
+  title: '"QgasSP/TransportBaseline',
+  component: TransportBaseline,
+  args: {},
+};
+
+const Template = (args) => <TransportBaseline {...args} />;
+
+export const BaselineInput = Template.bind({});
+BaselineInput.args = {
+  primary: true,
+  label: "TransportBaseline",
+};
