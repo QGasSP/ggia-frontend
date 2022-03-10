df9f897515844babd75d92fa839d09550df17933
Onunda <vonunda@gmail.com>
2022-03-10 11:05:08 +0200

    [U2 planner] format and remove redundant code

--- frontend_qgassp/src/components/U2planner.jsx
+++ frontend_qgassp/src/components/U2planner.jsx
@@ -4,12 +4,10 @@ import { U2legend } from "./U2legend";
 import axios from "axios";
 import "../css/u2planner.css";
 import { Button } from "./Button";
-import { useNavigate } from "react-router-dom";
 import Chip from "@mui/material/Chip";
 import Divider from "@mui/material/Divider";
 import {
   XYPlot,
-  AreaSeries,
   XAxis,
   YAxis,
   HorizontalGridLines,
@@ -79,7 +77,6 @@ export const U2planner = ({
         </div>
 
         <section>
-          {/*  <div>{newPopulation}</div> */}
           <form>
             <label>
               <b>New residents</b>
@@ -139,82 +136,6 @@ export const U2planner = ({
               <label>{settlementDistribution.rural}</label>
               <label>{newDevelopment.newSettlementDistribution.rural}</label>
             </div>
-            
-            {/* {JSON.stringify(projections.population)}
-            {JSON.stringify(newDevelopment.impact.population)} */}
-
-            {/* <XYPlot>
-              <AreaSeries
-                data={[
-                  { x: 2022, y: newPopulation[2022] },
-                  { x: 2023, y: newPopulation[2023] },
-                  { x: 2024, y: newPopulation[2024] },
-                  { x: 2025, y: newPopulation[2025] },
-                  { x: 2026, y: newPopulation[2026] },
-                  { x: 2027, y: newPopulation[2027] },
-                  { x: 2028, y: newPopulation[2028] },
-                  { x: 2029, y: newPopulation[2029] },
-                  { x: 2030, y: newPopulation[2030] },
-                  { x: 2031, y: newPopulation[2031] },
-                  { x: 2032, y: newPopulation[2032] },
-                  { x: 2033, y: newPopulation[2033] },
-                  { x: 2034, y: newPopulation[2034] },
-                  { x: 2035, y: newPopulation[2035] },
-                  { x: 2036, y: newPopulation[2036] },
-                  { x: 2037, y: newPopulation[2037] },
-                  { x: 2038, y: newPopulation[2038] },
-                  { x: 2039, y: newPopulation[2039] },
-                  { x: 2040, y: newPopulation[2040] },
-                  { x: 2041, y: newPopulation[2041] },
-                  { x: 2042, y: newPopulation[2042] },
-                  { x: 2043, y: newPopulation[2043] },
-                  { x: 2044, y: newPopulation[2044] },
-                  { x: 2045, y: newPopulation[2045] },
-                  { x: 2046, y: newPopulation[2046] },
-                  { x: 2047, y: newPopulation[2047] },
-                  { x: 2048, y: newPopulation[2048] },
-                  { x: 2049, y: newPopulation[2049] },
-                  { x: 2050, y: newPopulation[2050] },
-                ]}
-                opacity={0.25}
-                style={{}}
-              />
-              <AreaSeries
-                data={[
-                  { x: 2022, y: baselinePopulation[2022] },
-                  { x: 2023, y: baselinePopulation[2023] },
-                  { x: 2024, y: baselinePopulation[2024] },
-                  { x: 2025, y: baselinePopulation[2025] },
-                  { x: 2026, y: baselinePopulation[2026] },
-                  { x: 2027, y: baselinePopulation[2027] },
-                  { x: 2028, y: baselinePopulation[2028] },
-                  { x: 2029, y: baselinePopulation[2029] },
-                  { x: 2030, y: baselinePopulation[2030] },
-                  { x: 2031, y: baselinePopulation[2031] },
-                  { x: 2032, y: baselinePopulation[2032] },
-                  { x: 2033, y: baselinePopulation[2033] },
-                  { x: 2034, y: baselinePopulation[2034] },
-                  { x: 2035, y: baselinePopulation[2035] },
-                  { x: 2036, y: baselinePopulation[2036] },
-                  { x: 2037, y: baselinePopulation[2037] },
-                  { x: 2038, y: baselinePopulation[2038] },
-                  { x: 2039, y: baselinePopulation[2039] },
-                  { x: 2040, y: baselinePopulation[2040] },
-                  { x: 2041, y: baselinePopulation[2041] },
-                  { x: 2042, y: baselinePopulation[2042] },
-                  { x: 2043, y: baselinePopulation[2043] },
-                  { x: 2044, y: baselinePopulation[2044] },
-                  { x: 2045, y: baselinePopulation[2045] },
-                  { x: 2046, y: baselinePopulation[2046] },
-                  { x: 2047, y: baselinePopulation[2047] },
-                  { x: 2048, y: baselinePopulation[2048] },
-                  { x: 2049, y: baselinePopulation[2049] },
-                  { x: 2050, y: baselinePopulation[2050] },
-                ]}
-                opacity={0.25}
-                style={{}}
-              />
-            </XYPlot> */}
 
             <XYPlot
               width={900}
@@ -309,36 +230,6 @@ export const U2planner = ({
                   { x: 2048, y: projections.population[2048] },
                   { x: 2049, y: projections.population[2049] },
                   { x: 2050, y: projections.population[2050] },
-
-                  // { x: 2022, y: baselinePopulation[2022] },
-                  // { x: 2023, y: baselinePopulation[2023] },
-                  // { x: 2024, y: baselinePopulation[2024] },
-                  // { x: 2025, y: baselinePopulation[2025] },
-                  // { x: 2026, y: baselinePopulation[2026] },
-                  // { x: 2027, y: baselinePopulation[2027] },
-                  // { x: 2028, y: baselinePopulation[2028] },
-                  // { x: 2029, y: baselinePopulation[2029] },
-                  // { x: 2030, y: baselinePopulation[2030] },
-                  // { x: 2031, y: baselinePopulation[2031] },
-                  // { x: 2032, y: baselinePopulation[2032] },
-                  // { x: 2033, y: baselinePopulation[2033] },
-                  // { x: 2034, y: baselinePopulation[2034] },
-                  // { x: 2035, y: baselinePopulation[2035] },
-                  // { x: 2036, y: baselinePopulation[2036] },
-                  // { x: 2037, y: baselinePopulation[2037] },
-                  // { x: 2038, y: baselinePopulation[2038] },
-                  // { x: 2039, y: baselinePopulation[2039] },
-                  // { x: 2040, y: baselinePopulation[2040] },
-                  // { x: 2041, y: baselinePopulation[2041] },
-                  // { x: 2042, y: baselinePopulation[2042] },
-                  // { x: 2043, y: baselinePopulation[2043] },
-                  // { x: 2044, y: baselinePopulation[2044] },
-                  // { x: 2045, y: baselinePopulation[2045] },
-                  // { x: 2046, y: baselinePopulation[2046] },
-                  // { x: 2047, y: baselinePopulation[2047] },
-                  // { x: 2048, y: baselinePopulation[2048] },
-                  // { x: 2049, y: baselinePopulation[2049] },
-                  // { x: 2050, y: baselinePopulation[2050] },
                 ]}
                 opacity={1}
                 stroke="rgba(102,116,155,1)"
@@ -351,15 +242,6 @@ export const U2planner = ({
 
             <U2legend />
 
-            {/*  <div className="backButtonNew">
-              <Button
-                size="small"
-                value="backProjections"
-                onClick={() => navigate(-1)}
-                label="&laquo; Previous"
-                secondary
-              />
-            </div> */}
             <div className="nextU2Button">
               <Button
                 size="small"
@@ -384,7 +266,7 @@ export const U2planner = ({
     );
   }
 };
-// baseline, newDevelopment, emission, projections
+
 U2planner.propTypes = {
   emission: PropTypes.object.isRequired,
   projections: PropTypes.object.isRequired,
