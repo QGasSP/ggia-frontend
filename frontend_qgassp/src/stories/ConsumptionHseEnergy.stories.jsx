e7b98e8df7a5c27b808dd1a4d8b41338e7314169
Onunda <vonunda@gmail.com>
2022-03-22 16:04:46 +0200

    [Counsumption-based] add missing story

---
+++ frontend_qgassp/src/stories/ConsumptionHseEnergy.stories.jsx
@@ -0,0 +1,18 @@
+import { ConsumptionHseEnergy } from "../components/ConsumptionHseEnergy";
+import React from "react";
+
+export default {
+  title: '"QgasSP/ConsumptionHseEnergy',
+  component: ConsumptionHseEnergy,
+  args: {},
+};
+
+const Template = (args) => <ConsumptionHseEnergy {...args} />;
+
+export const ConsumptionEnergy = Template.bind({});
+ConsumptionEnergy.args = {
+  primary: true,
+  label: "ConsumptionHseEnergy",
+};
+
+
