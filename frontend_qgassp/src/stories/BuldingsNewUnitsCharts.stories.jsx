79b158cfa37c219b0b2ff84e124f5dd37219a4c4
manafov <matin.manafovvvv@gmail.com>
2022-03-23 16:28:48 +0200

    buildings new units charts page added

---
+++ frontend_qgassp/src/stories/BuldingsNewUnitsCharts.stories.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {BuldingsNewUnitsCharts} from "../components/BuldingsNewUnitsCharts";
+
+export default {
+  title: '"QgasSP/BuldingsNewUnitsCharts',
+  component: BuldingsNewUnitsCharts,
+  args: {},
+};
+
+const Template = (args) => <BuldingsNewUnitsCharts {...args} />;
+
+export const NewUnitsCharts = Template.bind({});
+NewUnitsCharts.args = {
+  primary: true,
+  label: "BuldingsNewUnitsCharts",
+};
